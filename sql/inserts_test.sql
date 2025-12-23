a1677070bbee88a617666f593839143978444e88
TM-Albert <albertgrzegrzolka124@gmail.com>
2025-12-23 21:19:13 +0100

    deleted ids from queries

--- sql/inserts_test.sql
+++ sql/inserts_test.sql
@@ -1,43 +1,46 @@
-INSERT INTO Voivodeships (id, name) VALUES
-(1, 'Dolnośląskie'),
-(2, 'Kujawsko-Pomorskie'),
-(3, 'Lubelskie'),
-(4, 'Lubuskie'),
-(5, 'Łódzkie'),
-(6, 'Małopolskie'),
-(7, 'Mazowieckie'),
-(8, 'Opolskie'),
-(9, 'Podkarpackie'),
-(10, 'Podlaskie'),
-(11, 'Pomorskie'),
-(12, 'Śląskie'),
-(13, 'Świętokrzyskie'),
-(14, 'Warmińsko-Mazurskie'),
-(15, 'Wielkopolskie'),
-(16, 'Zachodniopomorskie');
+INSERT INTO voivodeships (name) VALUES
+('Dolnośląskie'),
+('Kujawsko-Pomorskie'),
+('Lubelskie'),
+('Lubuskie'),
+('Łódzkie'),
+('Małopolskie'),
+('Mazowieckie'),
+('Opolskie'),
+('Podkarpackie'),
+('Podlaskie'),
+('Pomorskie'),
+('Śląskie'),
+('Świętokrzyskie'),
+('Warmińsko-Mazurskie'),
+('Wielkopolskie'),
+('Zachodniopomorskie');
 
-INSERT INTO School_profiles (id, name) VALUES
-(1, 'Technik Programista'),
-(2, 'Technik Informatyk'),
-(3, 'Technik Elektronik'),
-(4, 'Technik Mechatronik'),
-(5, 'Technik Ekonomista'),
-(6, 'Technik Logistyk'),
-(7, 'Technik Budownictwa'),
-(8, 'Gastronomia'),
-(9, 'Hotelarstwo'),
-(10, 'Liceum Ogólnokształcące');
 
-INSERT INTO Departments (id, name) VALUES
-(1, 'IT'),
-(2, 'Administracja'),
-(3, 'Finanse'),
-(4, 'Kadry'),
-(5, 'Edukacja'),
-(6, 'Techniczne'),
-(7, 'Gastronomia');
+INSERT INTO school_profiles (name) VALUES
+('Technik Programista'),
+('Technik Informatyk'),
+('Technik Elektronik'),
+('Technik Mechatronik'),
+('Technik Ekonomista'),
+('Technik Logistyk'),
+('Technik Budownictwa'),
+('Gastronomia'),
+('Hotelarstwo'),
+('Liceum Ogólnokształcące');
 
-INSERT INTO Currencies (id, value) VALUES
-(1, 'PLN'),
-(2, 'EUR'),
-(3, 'USD');
+
+INSERT INTO departments (name) VALUES
+('IT'),
+('Administracja'),
+('Finanse'),
+('Kadry'),
+('Edukacja'),
+('Techniczne'),
+('Gastronomia');
+
+
+INSERT INTO currencies (value) VALUES
+('PLN'),
+('EUR'),
+('USD');
