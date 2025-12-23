2e7e7ece72c06050d8afea856a3f9326c401343f
TM-Albert <albertgrzegrzolka124@gmail.com>
2025-12-23 21:27:08 +0100

    Added 3 schools insert script

--- sql/inserts_test.sql
+++ sql/inserts_test.sql
@@ -44,3 +44,50 @@ INSERT INTO currencies (value) VALUES
 ('PLN'),
 ('EUR'),
 ('USD');
+
+
+INSERT INTO School (
+    name,
+    number,
+    voivodeship_id,
+    city,
+    street,
+    post_code,
+    secretary_phone,
+    director_id,
+    profile_id
+)
+VALUES
+(
+    'Zespół Szkół Technicznych',
+    101,
+    7,
+    'Warszawa',
+    'ul. Nowoczesna 10',
+    '00-001',
+    '+48 221 224 567',
+    NULL,
+    1
+),
+(
+    'Technikum Informatyczne',
+    202,
+    12,
+    'Katowice',
+    'ul. Techniczna 5',
+    '40-001',
+    '+48 329 876 543',
+    NULL,
+    2
+),
+(
+    'Szkoła Branżowa Gastronomiczna',
+    303,
+    16,
+    'Szczecin',
+    'ul. Smakoszy 8',
+    '70-001',
+    '+48 915 554 433',
+    NULL,
+    8
+);
